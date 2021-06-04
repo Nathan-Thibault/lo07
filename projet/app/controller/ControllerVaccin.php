7c8b0ae1b387f345260b8855c20c4a0f232ce20b
Ambroide THIBAULT <thibaultambroise24@gmail.com>
2021-06-04 09:55:11 +0200

    Ajout d'une méthode vaccinCreate pour afficher un formulaire d'ajout. Ajout d'une autre méthode vaccinCreated pour insérer ce vaccin dans la base de donnée

--- projet/app/controller/ControllerVaccin.php
+++ projet/app/controller/ControllerVaccin.php
@@ -13,8 +13,8 @@ class ControllerVaccin {
             echo ("ControllerVaccin : vaccinationAccueil : vue = $vue");
         require ($vue);
     }
-    
-        // --- Liste des vins
+
+    // --- Liste des vins
     public static function vaccinReadAll() {
         $results = ModelVaccin::getAll();
         // ----- Construction chemin de la vue
@@ -22,6 +22,24 @@ class ControllerVaccin {
         $vue = $root . '/app/view/vaccin/viewAll.php';
         if (DEBUG)
             echo ("ControllerVaccin : vinReadAll : vue = $vue");
+        require ($vue);
+    }
+
+    // Ajout d'un vaccin via un formulaire
+    public static function vaccinCreate() {
+        // ----- Construction chemin de la vue vers un formulaire pour entrer données des vaccins
+        include 'config.php';
+        $vue = $root . '/app/view/vaccin/viewInsert.php';
+        require ($vue);
+    }
+
+    public static function vaccinCreated() {
+        $results = ModelVaccin::insert(
+                        htmlspecialchars($_GET['label']), htmlspecialchars($_GET['doses'])
+        );
+        // ----- Construction chemin de la vue
+        include 'config.php';
+        $vue = $root . '/app/view/vaccin/viewInserted.php';
         require ($vue);
     }
 
