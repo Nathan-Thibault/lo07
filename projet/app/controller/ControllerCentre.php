66e47623047a43ffb15c92f292d36903b4bb4029
Ambroide THIBAULT <thibaultambroise24@gmail.com>
2021-06-05 15:26:29 +0200

    ajout 2 méthode pour ajouter un centre : centreCreate et centreCreated

--- projet/app/controller/ControllerCentre.php
+++ projet/app/controller/ControllerCentre.php
@@ -25,6 +25,24 @@ class ControllerCentre {
         require ($vue);
     }
 
+    // Ajout d'un centre via un formulaire
+    public static function centreCreate() {
+        // ----- Construction chemin de la vue vers un formulaire pour entrer données des vaccins
+        include 'config.php';
+        $vue = $root . '/app/view/centre/viewInsert.php';
+        require ($vue);
+    }
+
+    public static function centreCreated() {
+        $results = ModelCentre::insert(
+                        htmlspecialchars($_GET['label']), htmlspecialchars($_GET['adresse'])
+        );
+        // ----- Construction chemin de la vue
+        include 'config.php';
+        $vue = $root . '/app/view/centre/viewInserted.php';
+        require ($vue);
+    }
+
 }
 ?>
 <!-- ----- fin ControllerCentre -->
