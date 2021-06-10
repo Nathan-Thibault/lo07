4e87f0d8a3927e075d072e20d75448f2b6ae6dcf
Ambroide THIBAULT <thibaultambroise24@gmail.com>
2021-06-10 09:27:09 +0200

    mis à jour du ControllerVaccination pour rediriger vers les pages de documentation

--- projet/app/controller/ControllerVaccination.php
+++ projet/app/controller/ControllerVaccination.php
@@ -1,12 +1,46 @@
 <!-- ----- debut ControllerVaccination -->
 <?php
+
 class ControllerVaccination {
+
     // --- page d'acceuil
     public static function vaccinationAccueil() {
         include 'config.php';
         $vue = $root . '/app/view/viewVaccinationAccueil.php';
         if (DEBUG)
             echo ("ControllerVaccination : vaccinationAccueil : vue = $vue");
+        require ($vue);
+    }
+
+    public static function documentationInnovation1() {
+        include 'config.php';
+        $vue = $root . '/public/documentation/innovationSuspensionVaccin.php';
+        if (DEBUG)
+            echo ("ControllerVaccination : documentationInnovation1 : vue = $vue");
+        require ($vue);
+    }
+    
+    public static function documentationInnovation2() {
+        include 'config.php';
+        $vue = $root . '/public/documentation/innovationTransfertStock.php';
+        if (DEBUG)
+            echo ("ControllerVaccination : documentationInnovation2 : vue = $vue");
+        require ($vue);
+    }
+    
+    public static function documentationInnovation3() {
+        include 'config.php';
+        $vue = $root . '/public/documentation/innovationChoixVaccin.php';
+        if (DEBUG)
+            echo ("ControllerVaccination : documentationInnovation3 : vue = $vue");
+        require ($vue);
+    }
+
+    public static function pointDeVueProjet() {
+        include 'config.php';
+        $vue = $root . '/public/documentation/pointDeVueProjet.php';
+        if (DEBUG)
+            echo ("ControllerVaccination : pointDeVueProjet : vue = $vue");
         require ($vue);
     }
 }
