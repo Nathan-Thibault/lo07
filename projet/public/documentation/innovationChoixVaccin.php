c80bc1a5860b658f0bdc40e6ae42c96a801ba052
Ambroide THIBAULT <thibaultambroise24@gmail.com>
2021-06-10 09:26:13 +0200

    Ajout des 4 fichiers pour affichage de la documentation

---
+++ projet/public/documentation/innovationChoixVaccin.php
@@ -0,0 +1,30 @@
+
+
+
+<?php
+require ($root . '/app/view/fragment/fragmentVaccinationHeader.html');
+?>
+
+<body>
+    <div class="container">
+        <?php
+        include $root . '/app/view/fragment/fragmentVaccinationMenu.html';
+        include $root . '/app/view/fragment/fragmentVaccinationJumbotron.html';
+        ?>
+
+        <div class="panel panel-primary">
+            <div class="panel-heading">Documentation Innovation Choix vaccin</div>
+            <div class="panel-body">
+                <p>À compléter</p>
+            </div>
+        </div>
+
+
+    </div>
+    <?php include $root . '/app/view/fragment/fragmentVaccinationFooter.html'; ?>
+
+
+
+
+
+
